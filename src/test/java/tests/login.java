bc7fbc91012c63101e0627dbd3682b54c5e9d851
tasyseby <57195437+tasyseby@users.noreply.github.com>
2019-12-16 20:24:44 +0000

    Update login.java

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -5,5 +5,8 @@ public class login {
     public static void main(String[] args) {
 
             //add from GitHub HelloWord2
+        
+        
+        String lastUpdate = "HELLO AGAIN FROM GIT";
     }
 }
